367b3d45255552c30e4dc9ee91b7a3708af9963a
Isabel Uriarte <uriarte.isabel@gmail.com>
2020-01-08 13:26:06 -0800

    wip

--- index.js
+++ index.js
@@ -1,47 +1,64 @@
 const express = require("express")
+const path = require("path")
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3004;
+
+
+const reservations ={
+    tables: [],
+    waitlist: []
+}
 
 app.get("/", (req, res) => {
-    res.send("Welcome to my API");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/tables", (req, res) => {
-    // res.json({
-    //     name: "spinach",
-    //     bestFood: true
-    // });
+    // res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/reserve", (req, res) => {
-    // res.json({
-    //     name: "spinach",
-    //     bestFood: true
-    // });
+    // res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/api/tables", (req, res) => {
-    // res.json({
-    //     name: "spinach",
-    //     bestFood: true
-    // });
-});
-
-app.get("/api/tables", (req, res) => {
-    // res.json({
-    //     name: "spinach",
-    //     bestFood: true
-    // });
+    res.json(reservations.tables);
 });
 
 app.get("/api/waitlist", (req, res) => {
-    // res.json({
-    //     name: "spinach",
-    //     bestFood: true
-    // });
+    res.json(reservations.waitlist);
 });
+
+// Displays all tables
+app.get("/api/tables", function(req, res) {
+    return res.json(reservations);
+  });
+
+// Create New Tables - takes in JSON input
+app.post("/api/tables", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newTable = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    // newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newTable);
+  
+    if (tables.length <= 5){
+        reservations.tables.push(newTable);
+    } else {
+        reservations.waitlist.push(newTable);
+    }
+
+    res.json(newTable);
+  });
 
 app.listen(PORT, function(){
     console.log("listening on port" + PORT);
