ac303a8bce761555f36df6eb230a606deebe2b00
patorrad <paolo.a.torrado@gmail.com>
2020-01-08 17:05:14 -0800

    Addressed for loop issue

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ const express = require("express")
 const path = require("path")
 
 const app = express();
+const PORT = process.env.PORT || 3004;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const PORT = process.env.PORT || 3004;
 
-// const reservations = {
-    const tables = [];
-    const waitlist = [];
-// }
+
+const reservations = {
+    tables: [],
+    waitlist: []
+}
 
 
 app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+app.get("/home", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
@@ -34,11 +38,6 @@ app.get("/api/waitlist", (req, res) => {
     res.json(reservations.waitlist);
 });
 
-// Displays all tables
-// app.get("/api/tables", function(req, res) {
-//     return res.json(reservations);
-//   });
-
 // Create New Tables - takes in JSON input
 app.post("/api/tables", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
@@ -48,15 +47,13 @@ app.post("/api/tables", function(req, res) {
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
     // newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
-  
-    console.log(newTable);
-
-    if (tables.length <= 5){
-        tables.push(newTable);
+    
+    if (reservations.tables.length < 5)
+    {
+        reservations.tables.push(newTable);
     } else {
-        waitlist.push(newTable);
+        reservations.waitlist.push(newTable);
     }
-
     res.json(newTable);
   });
 
